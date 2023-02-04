d93a739c53d4149aed8b6e57241bb2dc856d153d
matt <mat@unc.edu>
2023-02-04 18:45:38 -0500

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,10 +19,10 @@ const Navbar = ({active}) => {
             margin: '0 auto',
             marginBottom: '20px'
         }}>
-            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 0.5s ease-in-out' }}>Home</p> </Link>
-            {active!='p' && <Link to="/projects" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 0.9s ease-in-out' }}>Projects</p> </Link>}
-            {active!='b' && <Link to="/businesses" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 1.3s ease-in-out' }}>Businesses</p> </Link>}
-            {active!='c' && <Link to="/connect" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 1.7s ease-in-out' }}>Connect</p> </Link>}
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 0.9s ease-in-out' }}>Home</p> </Link>
+            {active!='p' && <Link to="/projects" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 1.3s ease-in-out' }}>Projects</p> </Link>}
+            {active!='b' && <Link to="/businesses" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 1.7s ease-in-out' }}>Businesses</p> </Link>}
+            {active!='c' && <Link to="/connect" style={{ textDecoration: 'none', color: 'white' }}> <p style={{ fontSize: '1.2rem', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 2.1s ease-in-out' }}>Connect</p> </Link>}
         </div>
     );
 }
