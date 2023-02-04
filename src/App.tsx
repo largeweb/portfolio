ffe6106ab24174417fc4e1520c8c6dd9492d4aa0
matt <mat@unc.edu>
2023-02-04 16:38:06 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
       <header className="App-header">
+        <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=0.7" />
+        </Helmet>
         <h1>Web3, Blockchain & The Metaverse</h1>
         <nav>
           <button
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,22 +10,25 @@ const Navbar = () => {
             backgroundColor: 'lightgray',
             borderRadius: '10px',
             // color: 'white',
-            marginBottom: '20px',
             height: '60px',
+            maxWidth: '1100px',
+            width: '100%',
             animation: 'navbarFadeIn 1s ease-in-out',
-            boxShadow: '0px 5px 10px #c0c0c0'
+            boxShadow: '0px 5px 10px #c0c0c0',
+            margin: '0 auto',
+            marginBottom: '20px'
         }}>
             <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-                <p style={{ fontSize: '20px', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 1s ease-in-out' }}>Home</p>
+                <p style={{ fontSize: '1rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 0.5s ease-in-out' }}>Home</p>
             </Link>
             <Link to="/projects" style={{ textDecoration: 'none', color: 'white' }}>
-                <p style={{ fontSize: '20px', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 1.5s ease-in-out' }}>Projects</p>
+                <p style={{ fontSize: '1rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 0.9s ease-in-out' }}>Projects</p>
             </Link>
             <Link to="/businesses" style={{ textDecoration: 'none', color: 'white' }}>
-                <p style={{ fontSize: '20px', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 2s ease-in-out' }}>Businesses</p>
+                <p style={{ fontSize: '1rem', color:"gray", padding:"10px", animation: 'navbarButtonFadeIn 1.3s ease-in-out' }}>Businesses</p>
             </Link>
             <Link to="/connect" style={{ textDecoration: 'none', color: 'white' }}>
-                <p style={{ fontSize: '20px', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 2.5s ease-in-out' }}>Connect</p>
+                <p style={{ fontSize: '1rem', color:"gray", padding:"20px", animation: 'navbarButtonFadeIn 1.7s ease-in-out' }}>Connect</p>
             </Link>
         </div>
     );
