6912a9e414948a1d6b44f654a94f0ffea8a3ea93
matt <mat@unc.edu>
2023-02-08 19:52:16 -0500

    test

--- src/pages/Connect.tsx
+++ src/pages/Connect.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const Connect = () => {
+
+    const [m, sm] = React.useState('');
+
     return (
         <div className="connect">
             <div style={{position:"fixed",top:"0",left:"0",bottom:"0",right:"0",backgroundColor:"lightgray",zIndex:"-2"}}></div>
             <Navbar active={'c'}/>
             <h1>Connect with me</h1>
+            {/* Text Area for value m */}
+            <textarea className='connectTextArea ' style={{background:"lightgray",color:"black",fontSize:"18px"}} placeholder='Leave a Message?' value={m} onChange={(e) => sm(e.target.value)}></textarea>
+            {1 && <button style={{outline:"none"}} className={m ? 'connectSend' : 'connectSendInvisible'}>Send</button>}
             {/* <div className="connect-links">
                 <Link to="/"><button>Back</button></Link>
             </div> */}
